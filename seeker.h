15c182b6e88e93bb03c2c1e8fe2e73a532697f85
SadMonk <SadMonk@gmx.de>
2019-05-08 00:46:52 +0200

    we want vector objects for pos, acc and vel... also override shape and boundingrect functions

--- seeker.h
+++ seeker.h
@@ -2,22 +2,24 @@
 #define SEEKER_H
 
 #include <QGraphicsItem>
+#include <QVector2D>
 
 class Seeker : public QGraphicsItem
 {
 public:
     Seeker();
 
+    QRectF boundingRect() const override;
+    QPainterPath shape() const override;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
 
 protected:
     void advance(int step) override;
 
 private:
-    int x;
-    int y;
-    int accX;
-    int accY;
+    QVector2D position;
+    QVector2D velocity;
+    QVector2D acceleration;
 };
 
 #endif // SEEKER_H
