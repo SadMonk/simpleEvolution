81cf3dd01671a31eb2eca91eab9a237ff0fef7cd
SadMonk <SadMonk@gmx.de>
2019-05-10 00:38:33 +0200

    cast stuff around so that we don't get any compiler warnings (am I just hiding unprecise calculations here?)

--- seeker.cpp
+++ seeker.cpp
@@ -11,7 +11,10 @@ Seeker::Seeker()
     // for now, initialize in the middle of the screen with no velocity or acceleration
     position = QVector2D(0,0);
     velocity = QVector2D(0,0);
-    acceleration = QVector2D((-50 + QRandomGenerator::global()->bounded(100.0)) / 100.0,(-50 + QRandomGenerator::global()->bounded(100)) / 100.0);
+    acceleration = QVector2D(QPointF(
+                                 (-50 + QRandomGenerator::global()->bounded(100.0)) / 100.0,
+                                 (-50 + QRandomGenerator::global()->bounded(100)) / 100.0)
+                             );
 }
 
 // pretty simple paint... just paint a round dot.
